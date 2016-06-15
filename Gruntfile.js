01f441f92efc6e1c65d8fd732fe5dc92950adbc4
YongWoo Jeon <yongwoo.jeon@navercorp.com>
2016-06-15 11:57:13 +0900

    Generate doc #3

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function( grunt ) {
+	var clc = require('cli-color');
+	var pack = grunt.file.readJSON("package.json");
+	var version = pack.version;
+	var src = 'v/'+version+'/src/';
+	var doc = 'v/'+version+'/doc/';
+	
+	grunt.loadNpmTasks('grunt-git');
+	grunt.loadNpmTasks('grunt-jsdoc');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	
+	grunt.initConfig({
+		gitclone : {
+			src : {
+				 options: {
+					repository: 'https://github.com/mixed/jsmocktool',
+					branch: 'master',
+	                directory: 'v/temp'
+	            }
+			}
+		},
+		copy: {
+			src: {
+				expand: true,
+				flatten: true,
+				src: 'v/temp/src/*',
+				dest: src,
+			}
+		},
+		clean: ['v/temp'],
+		jsdoc : {
+	        dist : {
+	            src: [src+'*.js', src+'*/*.js'],
+	            options: {
+	                destination: doc,
+					template : "node_modules/ink-docstrap/template",
+              		configure : "node_modules/ink-docstrap/template/jsdoc.conf.json"
+	            }
+	        }
+	    }
+	});
+
+	grunt.registerTask("default", ["gitclone:src", "copy:src", "clean","jsdoc:dist"]);
+
+}
